a2c2eac542c45feedd1b3efdacced8c4e6c47b55
Pepm17 <pablopupo1797@gmail.com>
2020-06-03 17:05:41 -0500

    Animation move for walk and fight

---
+++ Assets/Scripts/Character/AnimationMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationMove : MonoBehaviour
+{
+    private ThirdPersonMove thirdPersonMove;
+    private Animator animator;
+
+    private void Start()
+    {
+        this.animator = GetComponent<Animator>();
+        this.thirdPersonMove = GetComponent<ThirdPersonMove>();
+    }
+
+    void Update()
+    {
+        this.WalkAnimation();
+        this.AttackAnimation();
+    }
+
+    private void WalkAnimation()
+    {
+        if (this.thirdPersonMove.Direction.magnitude >= 0.1f)
+        {
+            this.animator.SetBool("Walk", true);
+        }
+        else
+        {
+            this.animator.SetBool("Walk", false);
+        }
+    }
+
+    private void AttackAnimation()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.animator.SetBool("Attack1", true);
+        }
+        else
+        {
+            this.animator.SetBool("Attack1", false);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            this.animator.SetBool("Attack2", true);
+        }
+        else
+        {
+            this.animator.SetBool("Attack2", false);
+        }
+    }
+}
--- Assets/Scripts/Character/ThirdPersonMove.cs
+++ Assets/Scripts/Character/ThirdPersonMove.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 
 public class ThirdPersonMove : MonoBehaviour
 {
-    public float speed = 6f;
-    public CharacterController controller;
     public Transform cam;
-    private float turnSmoothTime = 0.1f;
+    public float speed;    
+    public float turnSmoothTime;
     private float turnSmoothVelocity;
-    public Animator animator;
     private float horizontal;
     private float vertical;
-    private Vector3 direction; 
+    private Vector3 direction;
+    private CharacterController controller;
+
+    public Vector3 Direction { get => direction; }
+
+    private void Start()
+    {
+        this.controller = GetComponent<CharacterController>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         this.Move();
-        this.AnimationWalk();
     }
 
     private void Move()
@@ -28,25 +33,13 @@ public class ThirdPersonMove : MonoBehaviour
         this.vertical = Input.GetAxisRaw("Vertical");
         this.direction = new Vector3(this.horizontal, 0f, this.vertical).normalized;
 
-        if (direction.magnitude >= 0.1f)
+        if (Direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(Direction.x, Direction.z) * Mathf.Rad2Deg + this.cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref this.turnSmoothVelocity, this.turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
-        }
-    }
-
-    private void AnimationWalk()
-    {
-        if(this.direction.magnitude >= 0.1f)
-        {
-            this.animator.SetBool("Walk", true);
-        }
-        else
-        {
-            this.animator.SetBool("Walk", false);
         }
     }
 }
