f083868e1050baa750c4a180ea35422cee24f6cc
Pepm17 <pablopupo1797@gmail.com>
2020-06-03 18:17:46 -0500

    First enemy

---
+++ Assets/Scripts/Enemy/AnimationMoveEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationMoveEnemy : MonoBehaviour
+{
+    private Animator animator;
+    private CharacterController characterController;
+
+    private void Start()
+    {
+        this.animator = GetComponent<Animator>();
+        //this.characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        this.AttackAnimation();
+        this.Damage();
+    }
+
+    private void AttackAnimation()
+    {
+       
+    }
+
+    private void Damage()
+    {
+        
+    }
+}
